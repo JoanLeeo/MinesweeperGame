eb2ece985205694df1d79cefc41a7fe8a8e6f2b5
Mr_Wendao <liuchunhui@greatgate.com.cn>
2016-04-15 18:29:52 +0800

    initial

---
+++ MinesweeperGame/MineCellModel.m
@@ -0,0 +1,13 @@
+//
+//  MineCellModel.m
+//  MinesweeperGame
+//
+//  Created by PC-LiuChunhui on 16/4/15.
+//  Copyright © 2016年 Mr.Wendao. All rights reserved.
+//
+
+#import "MineCellModel.h"
+
+@implementation MineCellModel
+
+@end
---
+++ MinesweeperGame/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  MinesweeperGame
+//
+//  Created by PC-LiuChunhui on 16/4/15.
+//  Copyright © 2016年 Mr.Wendao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
